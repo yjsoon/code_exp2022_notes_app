34d662d5fd11379b18de02107cb272c06a761803
YJ Soon <yjsoon@tinkertanker.com>
2022-06-07 16:05:11 +0800

    Functionality to add new sample note

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Entypo } from "@expo/vector-icons";
@@ -9,15 +15,27 @@ import * as SQLite from "expo-sqlite";
 const db = SQLite.openDatabase("notes.db");
 
 function NotesScreen({ navigation }) {
+  // Create state variable for our notes
+  const [notes, setNotes] = useState([
+    { title: "Walk the dog", done: false, id: "0" },
+    { title: "Water the plants", done: false, id: "" },
+  ]);
+
+  function addNote() {
+    let newNote = {
+      title: "Sample new note",
+      done: false,
+      id: notes.length.toString(),
+    };
+    setNotes([...notes, newNote]);
+  }
+
+  // This adds the new note button in the header
   useEffect(() => {
     console.log("This effect happened!!!!");
     navigation.setOptions({
       headerRight: () => (
-        <TouchableOpacity
-          onPress={() => {
-            console.log("New note");
-          }}
-        >
+        <TouchableOpacity onPress={addNote}>
           <Entypo
             name="new-message"
             size={24}
@@ -29,9 +47,23 @@ function NotesScreen({ navigation }) {
     });
   });
 
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{ padding: 10, borderBottomColor: "#ccc", borderBottomWidth: 1 }}
+      >
+        <Text style={{ fontSize: 16 }}>{item.title}</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Notes</Text>
+      <FlatList
+        data={notes}
+        renderItem={renderItem}
+        style={{ width: "100%" }}
+      />
     </View>
   );
 }
