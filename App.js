a5a1661dd4c0fcbb08dfd48a593d2c9033ea13d6
YJ Soon <yjsoon@tinkertanker.com>
2022-06-07 15:56:52 +0800

    Added new note icon in header

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Entypo } from "@expo/vector-icons";
 
-function NotesScreen() {
+function NotesScreen({ navigation }) {
+  useEffect(() => {
+    console.log("This effect happened!!!!");
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          onPress={() => {
+            console.log("New note");
+          }}
+        >
+          <Entypo
+            name="new-message"
+            size={24}
+            color="black"
+            style={{ marginRight: 16 }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
   return (
     <View style={styles.container}>
       <Text>Notes</Text>
